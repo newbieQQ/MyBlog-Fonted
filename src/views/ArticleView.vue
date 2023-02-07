d8f595512dcb3bc348618275c057e5d420fab650
QQ <1770362456@qq.com>
2023-02-07 21:45:08 +0800

    update

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -2,17 +2,22 @@
   <div class="container">
     <div class="card">
       <div class="card-body">
-        这是文章
       </div>
     </div>
   </div>
 </template>
 
 <script>
+
+
+
+
+
+
+
 export default {
   name: 'ArticleView',
   components: {
-
   }
 }
 </script>
