f97db3ee0f85e9552e6b42e1c38742358b5f49f1
QQ <1770362456@qq.com>
2023-02-07 22:33:14 +0800

    update

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="container">
     <div class="card">
-      <div class="card-body">
+      <div class="card-body" id="ArticleContext">
       </div>
     </div>
   </div>
 </template>
 
 <script>
+const Http = new XMLHttpRequest();
+const url='http://119.29.52.203:8000/blog/1';
+Http.open("GET", url);
+Http.send();
 
-
-
-
+//Http.onreadystatechange = (e) => {
+//  console.log(Http.responseText)
+//}
 
 
 
@@ -21,3 +25,26 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+@media (min-width: 1025px){
+  .container {
+    margin-top: 20px;
+    width: 70%;
+  }
+}
+
+@media (max-width: 1025px) {
+  #UserinfoCard {
+    display: none;
+  }
+  #ArticleCard {
+    width: 100%;
+  }
+  .container {
+    margin-top: 20px;
+    width: 100%;
+  }
+}
+
+</style>
