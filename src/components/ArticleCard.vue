325f028780a5e63b9c2903c2091f35ce6a6edede
QQ <1770362456@qq.com>
2023-02-28 17:13:37 +0800

    实现父组件向子组件传递信息

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,111 +1,36 @@
 <template>
-  <ul>
-    <li class="card" v-for="blog in showblog" :key="blog.id">
-      <router-link :to="{name : 'article', params : {id : blog.id}}" class="page-link">
-        <div class="card-body">
-          <div id='ArticleTitle'>{{ blog.title }}</div>
-          <hr>
-            <div class="row">
-              <div class="col-sm-2">
-                <img class='img-fluid' src="https://qqpic-1312843796.cos.ap-guangzhou.myqcloud.com/Icpc_logo.png" alt="">
-              </div>
-              <div class="col-sm-6" style="margin-left:50px">
-                <div id='ArticleContent'>{{ blog.describe }}</div>
-              </div>
-            </div>
+<div class="card-body">
+  <router-link :to="{name : 'article', params : {id : blog.id}}" class="page-link">
+    <div id='ArticleTitle'>{{ blog.title }}</div>
+    <hr>
+      <div class="row">
+        <div class="col-sm-2">
+          <img class='img-fluid' src="https://qqpic-1312843796.cos.ap-guangzhou.myqcloud.com/Icpc_logo.png" alt="">
         </div>
-      </router-link>
-    </li>
-    <li>
-      <nav aria-label="Page navigation example">
-        <ul class="pagination">
-          <li class="page-item">
-            <div class="page-link" @click="prePage">
-              <span aria-hidden="true">&laquo;</span>
-            </div>
-          </li>
-          <li v-for="i in PageNum" :key="i">
-            <div class="page-link" @click="page(i)">
-              <span aria-hidden="true">{{ i }}</span>
-            </div>
-          </li>
-
-          <li class="page-item">
-            <div class="page-link" @click="nextPage">
-              <span aria-hidden="true">&raquo;</span>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </li>
-  </ul>
-
-
+        <div class="col-sm-6" style="margin-left:50px">
+          <div id='ArticleContent'>{{ blog.describe }}</div>
+        </div>
+      </div>
+  </router-link>
+</div>
 </template>
 
 
 <script>
-import $ from 'jquery';
 
 export default {
   name: 'ArticleCard',
   data() {
-    return {
-      CurrentPage: 0,
-      PageSize:    10,
-      PageNum:     1,
-      blogs:       [], // 总共blog
-      pageblog:    [], // 每一页的blog
-      showblog:    [], // 要显示的blog
-    }
   },
+  props: [ 
+    'blog',
+  ],
   created() {
-      $.ajax({
-        url : 'http://119.29.52.203:8000/blog/',
-        type : 'GET',
-        dataType : 'json',
-        success: (data) => {
-          for (var i = 0; i < data.length; i++) {
-            this.blogs.push(data[i]);
-          }
-          this.PageNum = Math.ceil(this.blogs.length / this.PageSize);
-          for (var j = 0; j < this.PageNum; j++) {
-            this.pageblog[j] = this.blogs.slice(this.PageSize * j, this.PageSize * ( j + 1 ));
-          }
-          this.showblog = this.pageblog[this.CurrentPage];
-        },
-        error: function() {
-          console.log("请求失败");
-        }
-      });
   },
-  methods:{
-      nextPage() {
-        if (this.CurrentPage === this.PageNum - 1) return ;
-        this.showblog = this.pageblog[++this.CurrentPage];
-      },
-      prePage() {
-        if (this.CurrentPage === 0) return ;
-        this.showblog = this.pageblog[--this.CurrentPage];
-      },
-      page(i) {
-          this.CurrentPage = i - 1;
-          this.showblog = this.pageblog[i - 1];
-      }
-  }
 }
 </script>
 
 <style scoped>
-li {
-  margin-bottom: 10px;
-  list-style: none;
-}
-
-.card-body {
-  height: 200px;
-}
-
 img {
   margin-top: 10px;
   float: left;
@@ -143,11 +68,5 @@ img {
 
 span{
   color: black;
-}
-
-.pagination {
-  width: 100%;
-  display: flex;
-  justify-content: center;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,37 @@
 <template>
   <div class="container">
-
     <div class="row">
-      <div class="col-sm-8" id="ArticleCard"> <ArticleCard/> </div>
+      <div class="col-sm-8" id="ArticleCard"> 
+        <ul>
+
+          <li class="card" v-for="blog in showblog" :key="blog.id">
+            <ArticleCard :blog='blog'/>
+          </li>
+
+          <li>
+            <nav aria-label="Page navigation example">
+              <ul class="pagination">
+                <li class="page-item">
+                  <div class="page-link" @click="prePage">
+                    <span aria-hidden="true">&laquo;</span>
+                  </div>
+                </li>
+                <li v-for="i in PageNum" :key="i">
+                  <div class="page-link" @click="page(i)">
+                    <span aria-hidden="true">{{ i }}</span>
+                  </div>
+                </li>
+                <li class="page-item">
+                  <div class="page-link" @click="nextPage">
+                    <span aria-hidden="true">&raquo;</span>
+                  </div>
+                </li>
+              </ul>
+            </nav>
+          </li>
+
+        </ul>
+      </div>
       <div class="col-sm-1" id="UserinfoCard"> <UserinfoCard/> </div>
     </div>
 
@@ -12,11 +41,58 @@
 <script>
 import ArticleCard from '../components/ArticleCard.vue';
 import UserinfoCard from '../components/UserinfoCard.vue';
+import $ from 'jquery';
+
 export default {
   name: 'HomeView',
   components: {
     ArticleCard,
     UserinfoCard,
+  }, 
+  created() {
+      $.ajax({
+        url : 'http://119.29.52.203:8000/blog/',
+        type : 'GET',
+        dataType : 'json',
+        success: (data) => {
+          console.log(data);
+          for (var i = 0; i < data.length; i++) {
+            this.blogs.push(data[i]);
+          }
+          this.PageNum = Math.ceil(this.blogs.length / this.PageSize);
+          for (var j = 0; j < this.PageNum; j++) {
+            this.pageblog[j] = this.blogs.slice(this.PageSize * j, this.PageSize * ( j + 1 ));
+          }
+          this.showblog = this.pageblog[this.CurrentPage];
+        },
+        error: function() {
+          console.log("请求失败");
+        }
+      });
+  },
+  data() {
+    return {
+      CurrentPage: 0,
+      PageSize:    10,
+      PageNum:     1,
+      blogs:       [], // 总共blog
+      pageblog:    [], // 每一页的blog
+      showblog:    [], // 要显示的blog
+    }
+  },
+  methods:{
+      nextPage() {
+        if (this.CurrentPage === this.PageNum - 1) return ;
+        this.showblog = this.pageblog[++this.CurrentPage];
+      },
+      prePage() {
+        if (this.CurrentPage === 0) return ;
+        this.showblog = this.pageblog[--this.CurrentPage];
+      },
+      page(i) {
+          this.CurrentPage = i - 1;
+          this.showblog = this.pageblog[i - 1];
+      }
   }
 }
 </script>
@@ -42,4 +118,18 @@ export default {
   }
 }
 
+li {
+  margin-bottom: 10px;
+  list-style: none;
+}
+
+.card-body {
+  height: 200px;
+}
+
+.pagination {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
 </style>
