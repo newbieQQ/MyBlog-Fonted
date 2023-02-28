c7ab53f559230a6e6da5f40d2bc140c81f8d9004
QQ <1770362456@qq.com>
2023-02-28 15:29:03 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+    {
+      'core-js': 3
+    }
   ]
 }
--- src/components/MarkdownView.vue
+++ src/components/MarkdownView.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="card">
     <div class="card-body">
-      {{markdown}}
+      <vue-makrdown :source="markdown"/>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
+  compontents: {
+  },
   data () {
     return {
-      markdown: `
-# 这是一个标题
-
-- 这是一个列表
-- 这是另一个列表
-
-[这是一个链接](https://www.bing.com)
-      `,
+      markdown: '#Hello Vue',
       html : ''
     }
   },
